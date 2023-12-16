9e7ea2f96a3e50267d99ab3cca3401f66afeebe7
mtuanvu <143334278+mtuanvu@users.noreply.github.com>
2023-12-16 17:43:21 +0700

    up

---
+++ app.js
@@ -0,0 +1,39 @@
+let fullName = "Mai Tuấn Vũ";
+let age = 18;
+let a = 4;
+let b = 10;
+
+// alert(fullName);
+// alert(age);
+
+// console.log();
+
+// confirm(fullName);
+
+// prompt(age);
+
+// setTimeout(function () {
+//   alert("Thong bao ve tin tuc Mai Tuan Vu dep trai nhat the gioi");
+// }, 1000);
+
+// setInterval(function () {
+//   console.log("Day la log" + Math.random);
+// }, 1000);
+
+// toán tử số học (arithmetic)
+// var c = a + b;
+// console.log(c);
+
+//toán tử gán (assignment)
+//var d = 4;
+// giá trị 4 được gán cho d
+
+//toán tử so sánh (compariaon)
+//if(a > b){
+//     alert ('Dung');
+// };
+
+//toán tử logic (logical)
+// if (a > 0 && b > 0) {
+//   alert("a va b deu la so lon hong 0"); toán tử && được thực thi khi tất cả điều kiện bên trong đều đúng
+// };
