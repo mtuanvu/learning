74f7ceb6952f12849d693bc8b0b167b9d32afb3b
mtuanvu <143334278+mtuanvu@users.noreply.github.com>
2023-12-18 01:01:15 +0700

    push

--- app.js
+++ app.js
@@ -1,43 +1,26 @@
-let fullName = "Mai Tuấn Vũ";
-// let age = 18;
-let a = 4;
-let b = 10;
+function getRandNumbers(min, max, length) {
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  var array = [];
+  for (var i = 0; i < length; i++) {
+    var randNumber = Math.floor(Math.random() * (max - min) + min);
+    array.push(randNumber);
+  }
+  return array;
+}
 
-// alert(fullName);
-// alert(age);
+const array = getRandNumbers(1, 94, 5);
+console.log(array);
 
-// console.log();
+function getTotal(numbers) {
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+  }
+  return total;
+}
 
-// confirm(fullName);
-
-// prompt(age);
-
-// setTimeout(function () {
-//   alert("Thong bao ve tin tuc Mai Tuan Vu dep trai nhat the gioi");
-// }, 1000);
-
-// setInterval(function () {
-//   console.log("Day la log" + Math.random);
-// }, 1000);
-
-// toán tử số học (arithmetic)
-// var c = a + b;
-// console.log(c);
-
-//toán tử gán (assignment)
-//var d = 4;
-// giá trị 4 được gán cho d
-
-//toán tử so sánh (compariaon)
-//if(a > b){
-//     alert ('Dung');
-// };
-
-//toán tử logic (logical)
-// if (a > 0 && b > 0) {
-//   alert("a va b deu la so lon hong 0"); toán tử && được thực thi khi tất cả điều kiện bên trong đều đúng
-// };
-
-let age = 35;
-let nextAge = ++age;
-console.log(nextAge);
+const myNumber = [1, 6, 8, 9, 10, 34];
+const sum = getTotal(myNumber);
+console.log(sum);
