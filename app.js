8beb6acc4d56845d1820d09f44200c7b93852f2b
mtuanvu <143334278+mtuanvu@users.noreply.github.com>
2023-12-18 01:38:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -68,3 +68,13 @@ console.log(sum);*/
 // getTotal(orders);
 
 // console.log(getTotal(orders));
+
+function run(object) {
+  var arr = [];
+  for (var key in object) {
+    arr.push(`Thuộc tính ${key} có giá trị ${object[key]}`);
+  }
+
+  return arr;
+}
+console.log(run({ name: "Nguyen Van A", age: 16 }));
