2c06f26ce030b65708a63c6810e5d6ae637684ab
mtuanvu <143334278+mtuanvu@users.noreply.github.com>
2023-12-19 01:25:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -151,3 +151,8 @@ console.log(sum);*/
 // }
 // console.log(getTotalGold(sports));
 var depthArray = [1, 2, [3, 4], 5, 6, [7, 8, 9]];
+var flatArray = depthArray.reduce(function (flatOutput, depItem) {
+  return flatOutput.concat(depItem);
+}, []);
+
+console.log(flatArray);
